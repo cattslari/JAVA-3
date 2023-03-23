821961a68e6bae9eeb6d3d0671732eb8474b7014
Larissa Silva 3 <114114544+cattslari@users.noreply.github.com>
2023-03-22 21:27:57 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Aula05/src/aula06/Aula06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Aula06 {
+
+    /**
+     * @param args the command line arguments
+     */
+}
