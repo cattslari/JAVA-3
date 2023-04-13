e94a734eeff0cebad19735e0af91d33dade0be40
Larissa Silva 3 <114114544+cattslari@users.noreply.github.com>
2023-04-12 21:52:56 -0300

    Amém, TERMINAMOS

---
+++ MinhaAplicacao/src/modelo/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    
+    public String getCpf(){
+        return cpf;
+    }
+
+public void setCpf(String cpf){
+    this.cpf = cpf;
+}
+public String getEmail(){
+        return email; 
+}
+
+public void setEmail(String email){
+    this.email = email;
+}
+public Long getId(){
+    return id;
+}
+
+public String getNome(){
+    return nome;
+}
+
+public void setNome(String nome){
+    this.nome= nome;
+}
+public String getTelefone(){
+    return telefone;
+}
+public void setTelefone(String telefone){
+    this.telefone = telefone;
+}
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
+
